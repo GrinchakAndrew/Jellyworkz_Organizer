77b931e6dd449bbfe601eb76c662500e2dda2ee9
GrinchakAndrew <andrewgrinchak@gmail.com>
2017-08-13 20:38:01 +0300

    init

---
+++ data/SDLC/data/_slides.js
@@ -0,0 +1,16 @@
+{
+	"collection" : ["data/SDLC/data/slide1.html", 
+					"data/SDLC/data/slide2.html", 
+					"data/SDLC/data/slide3.html", 
+					"data/SDLC/data/slide4.html", 
+					"data/SDLC/data/slide5.html", 
+					"data/SDLC/data/slide6.html", 
+					"data/SDLC/data/slide7.html", 
+					"data/SDLC/data/slide8.html",
+					"data/SDLC/data/slide9.html",
+					"data/SDLC/data/slide10.html",
+					"data/SDLC/data/slide11.html",
+					"data/SDLC/data/slide12.html",
+					"data/SDLC/data/slide13.html"
+					]
+}
---
+++ data/tasks.js
@@ -0,0 +1,26 @@
+{
+	" Positional Growth Blue Print" : [
+		{"Competency Modelling" : [
+					{"Developers" : {"(2) PHP Developer" : "data/php_developer/index.html", "(1) JavaScript Developer" : "data/javascriptdeveloper/index.html"}}
+				  ]
+		}
+	], 
+	
+	" Software Development Life-Cycle" : [
+					{"JellyWorkz SDLC " : [
+													{"SDLC Schema" : 
+														{ 
+														"(3) Flow Steps Description" : "data/SDLC/data/ProjectStepsDescription.txt", 
+														"(2) Project Mgmt Blocks" :  			"data/SDLC/data/project_life_cycle_blocks.html", 
+														"(1) SDLC Schema" : "data/SDLC/index.html"
+														}
+													}
+												]
+					}
+				  ]
+}
+
+
+
+
+
